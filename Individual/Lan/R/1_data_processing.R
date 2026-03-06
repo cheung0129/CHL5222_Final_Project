70aad4bd39f33a7186224f2e7d65cc167e47031c
Lan <hoanglan.tang@outlook.com>
2026-03-06 16:18:57 -0500

    datasets saved: processed, complete cases

--- Individual/Lan/R/1_data_processing.R
+++ Individual/Lan/R/1_data_processing.R
@@ -1,6 +1,7 @@
 # import packages
 library(here)
 library(dplyr)
+library(lubridate)
 
 # load data
 path_data_original <- here("data","original","ethiopia.csv")
@@ -30,18 +31,29 @@ data_processed <- data_original %>%
 
 
 # formatting
-data_processed <- data_original %>% 
-  mutate(yc = factor(yc, levels = c(1,0)),
+data_formatted <- data_processed %>% 
+  mutate(childid = factor(childid),
+         yc = factor(yc, levels = c(1,0)),
          round = factor(round, levels = c(1,2,3,4,5)),
-         dint = )
+         dint = lubridate::mdy(dint),
+         chsex = factor(chsex, levels = c(1,2)),
+         drwaterq_new = factor(drwaterq_new, levels = c(1,0)))
 
-# factor
-# - yc
-# - round
-# - chsex
-# - dfwaterq_new
-# 
-# datetime
-# - dint
+str(data_formatted)
+
 
 # save processed file
+save_path_processed <- here("data","processed","ethiopia_processed.csv")
+
+write.csv(data_formatted, save_path_processed)
+
+
+
+# data set with complete case analysis/listwise deletion
+data_complete_cases <- data_formatted[complete.cases(data_formatted),] 
+str(data_complete_cases)
+
+# save data set with removed missing observations
+save_path_complete_cases <- here("data","processed","ethiopia_complete_cases.csv")
+
+write.csv(data_complete_cases, save_path_complete_cases)
