d085cc5d56cb1f5f875db4691ce93887f5977936
wendy-shue <wendy.shue@mail.utoronto.ca>
2026-03-12 09:40:46 -0400

    Added figure by visit

---
+++ Individual/Wendy/Figure_by_visit.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(gtsummary)
+library(flextable)
+ethiopia <- read.csv("data/processed/ethiopia_processed.csv")
+
+
+ethiopia |> 
+  mutate(yc=factor(yc, levels=c(0,1), labels=c("Old","Young"))) |> 
+  ggplot(aes(x=agemon, y=zhfa,  col=round))+
+  geom_point()+
+  geom_violin(trim =F, fill=NA, col='red')+
+  facet_grid(round~yc, scales="free_x")+
+  theme_bw()+
+  labs(x="Age in months", y="Height-for-age z-score")
+ggsave("individual/Wendy/output/figure_by_visit.png", 
+       width=21,height=21,unit="cm")
