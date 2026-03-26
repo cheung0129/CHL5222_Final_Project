42829f99f6d672a0ff421819ef7ea4eea2dfea9a
wendy-shue <wendy.shue@mail.utoronto.ca>
2026-03-26 10:52:06 -0400

    updated figure by visit

--- Individual/Wendy/Figure_by_visit.R
+++ Individual/Wendy/Figure_by_visit.R
@@ -5,19 +5,31 @@ library(gtsummary)
 library(flextable)
 ethiopia <- read.csv("data/processed/ethiopia_processed.csv")
 ethiopia_cc <- read.csv("data/processed/ethiopia_complete_cases.csv")
-
-
 ethiopia_cc |> 
-  mutate(yc=factor(yc, levels=c(0,1), labels=c("Old","Young")),
-         drwaterq_new=as.factor(drwaterq_new)) |> 
-  ggplot(aes(x=round, y=zhfa, col=drwaterq_new))+
-  geom_violin(aes(group=round),fill=NA)+
-  facet_grid(yc~drwaterq_new,
-             labeller=as_labeller(c("0"="No safe drinking water","1"="Access to drinking water",
-                                    "Old"="Old", "Young"="Young")))+
-  stat_summary(geom="line",fun="median")+
-  theme_bw()+
-  labs(x="Survey Round", y="Height-for-age z-score")+
-  theme(legend.position = "none")
+  mutate(
+    yc = factor(yc, levels=c(0,1), labels=c("Old","Young")),
+    drwaterq_new = as.factor(drwaterq_new),
+    chsex = factor(chsex, levels=c(1,2), labels=c("F","M"))
+  ) |> 
+  # 1. Make 'round' a factor here so ggplot groups automatically
+  ggplot(aes(x = factor(round), y = zhfa, col = chsex)) +
+  
+  # 2. Remove group=round. Add position_dodge to place F/M side-by-side
+  geom_violin(fill = NA, position = position_dodge(width = 0)) +
+  
+  facet_grid(yc ~ drwaterq_new,
+             labeller = as_labeller(c("0"="No safe drinking water",
+                                      "1"="Access to drinking water",
+                                      "Old"="Old", 
+                                      "Young"="Young"))) +
+  
+  # 3. Add group=chsex to the line so it connects the medians correctly
+  stat_summary(aes(group = chsex), geom = "line", fun = "median", 
+               position = position_dodge(width = 0.7)) +
+  
+  theme_bw() +
+  labs(x = "Survey Round", y = "Height-for-age z-score") +
+  # 4. Changed from "none" to "bottom" so you can actually see the color labels!
+  theme(legend.position = "bottom")
 ggsave("individual/Wendy/output/figure_by_visit.png", 
        width=24,height=21,unit="cm")
