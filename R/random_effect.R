bfa6bd273754050f003b76ac2b8bfbf7d4e07a91
wendy-shue <wendy.shue@mail.utoronto.ca>
2026-03-27 18:07:28 -0400

    updated table random effect

--- R/random_effect.R
+++ R/random_effect.R
@@ -11,29 +11,27 @@ load("data/processed/lmmdata.RData")
 
 
 ## RI ====
-RI <- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon + chsex*drwaterq_new + agemon*drwaterq_new + chsex*agemon*drwaterq_new + (1 | childid), data=ethiopia_df)
+RI <- lmer(zhfa ~ chsex + time + drwaterq_new + age_first_c  + chsex*time + chsex*drwaterq_new + time*drwaterq_new + (1 | childid), data=ethiopia_df)
 
-## RI w/ yc
-RI_yc <- lmer(zhfa ~ chsex + agemon + yc + drwaterq_new + chsex*agemon + chsex*drwaterq_new + agemon*drwaterq_new + (1 | childid), data=ethiopia_df)
 
 ## RS w/ water ====
 ## reasoning of random slope for drwaterq_new: access to drinking water is not constant within each child 
-RS_water <- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon + chsex*drwaterq_new + agemon*drwaterq_new + (drwaterq_new | childid) , data=ethiopia_df)
+RS_water <- lmer(zhfa ~ chsex + time + drwaterq_new + age_first_c  + chsex*time + chsex*drwaterq_new + time*drwaterq_new + (drwaterq_new | childid) , data=ethiopia_df)
 
 ## RS w/ age ====
-RS_age <- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon + chsex*drwaterq_new + agemon*drwaterq_new + (agemon | childid), data=ethiopia_df)
+RS_age <- lmer(zhfa ~ chsex + time + drwaterq_new + age_first_c  + chsex*time + chsex*drwaterq_new + time*drwaterq_new + (time | childid), data=ethiopia_df)
 
 ## RS w/ age & water ====
-RS_age_water <- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon + chsex*drwaterq_new + agemon*drwaterq_new + chsex*agemon*drwaterq_new + (agemon + drwaterq_new | childid), data=ethiopia_df)
+RS_age_water <- lmer(zhfa ~ chsex + time + drwaterq_new + age_first_c  + chsex*time + chsex*drwaterq_new + time*drwaterq_new +  (time + drwaterq_new + age_first_c  | childid), data=ethiopia_df)
 
 
 # summary ====
 labels=c("RI","RS_water","RS_age", "RS_age_water")
 
-tab_model(RI,RS_water, RS_age, RS_age_water,
+tab_model(RI, RS_age,RS_water, RS_age_water,
           dv.labels = labels, 
           show.ci=F, show.se=T, show.aic=T,
-          file="output/Table_random_effect.doc",
+          show.r2 = F, show.obs=F,
+          file="output/table_random_effect.doc",
           p.style="star"
           )
-
