46af2edad087e0b22b882c50f3b5317114842cb9
Uvinnnn <cindychuan900928@gmail.com>
2026-04-03 17:53:29 -0400

    updating yc_oc comparison and interaction term selection

--- Individual/Uvin/yc_oc_analysis.R
+++ Individual/Uvin/yc_oc_analysis.R
@@ -1,91 +1,123 @@
 library(readr)
 library(tidyverse)
 library(lme4)
+
 library(sjPlot)
 
 ## Load dataset
-setwd("~/CHL5222_Final_Project")
-eth <- read_csv("https://raw.githubusercontent.com/cheung0129/CHL5222_Final_Project/refs/heads/main/data/processed/ethiopia_processed.csv")
+load("data/processed/lmmdata.RData")
 
-
-eth <- eth |>
-  mutate(chsex = factor(chsex,
-                        levels = c(1, 2)),
-         yc = factor(yc,
-                     levels = c(0, 1)),
-         round = factor(round),
-         drwaterq_new = factor(drwaterq_new,
-                               levels = c(0, 1)))
-#older cohort
-eth_oc <- eth |>
-  filter(yc == "0")
-#younger vohort
-eth_yc <- eth |>
-  filter(yc == "1")
 
 #################### full model ####################
-full_model<- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon +
-                    chsex*drwaterq_new + agemon*drwaterq_new +
-                    chsex*agemon*drwaterq_new +
+base_model<- lmer(zhfa ~ chsex + time + age_first_c + drwaterq_new +
+                    chsex*time*drwaterq_new +
                     (1 | childid),
-                  data = eth,
+                  data = ethiopia_df,
                   REML = FALSE)
+summary(base_model)
 
-
-interaction_model<- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon +
-                    chsex*drwaterq_new + agemon*drwaterq_new +
-                    chsex*agemon*drwaterq_new +
+interaction_model<- lmer(zhfa ~ chsex + time + age_first_c + drwaterq_new +
+                           chsex*time*drwaterq_new +
                     yc * (chsex + agemon + drwaterq_new) +
                     (1 | childid),
-                  data = eth,
+                  data = ethiopia_df,
                   REML = FALSE)
+summary(interaction_model)
 
 
-anova(full_model, interaction_model)
+anova(base_model, interaction_model)
 
 ####the effects of sex, age, and water quality
 ####differ between younger and older children 
 
 #################### old cohort ####################
-oc_model<- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon +
-                    chsex*drwaterq_new + agemon*drwaterq_new +
-                    chsex*agemon*drwaterq_new +
-                    (1 | childid),
-                  data = eth_oc,
-                  REML = FALSE)
+oc_model<- lmer(zhfa ~ chsex * time * drwaterq_new + age_first_c + 
+                  (1 | childid),
+                data = ethiopia_old,
+                REML = FALSE)
 summary(oc_model)
-oc_model_reduced <- update(oc_model, . ~ . - chsex:agemon:drwaterq_new)
+oc_model_reduced <- lmer(zhfa ~ chsex * time + chsex * drwaterq_new + time * drwaterq_new +
+                           age_first_c +
+                           (1 | childid),
+                         data = ethiopia_old,
+                         REML = FALSE)
 anova(oc_model, oc_model_reduced)
 
+#The three‑way interaction model provided a better fit to the oc data
+#Height for age differ by sex, water access and the combination of both
+##The slope over time is not the same for boys and girls, and that difference 
+#changes depending on whether the household has safe drinking water.
+
 #################### yound cohort ####################
-yc_model<- lmer(zhfa ~ chsex + agemon + drwaterq_new + chsex*agemon +
-                  chsex*drwaterq_new + agemon*drwaterq_new +
-                  chsex*agemon*drwaterq_new +
+yc_model<- lmer(zhfa ~ chsex * time * drwaterq_new + age_first_c +
                   (1 | childid),
-                data = eth_yc,
+                data = ethiopia_yg,
                 REML = FALSE)
 summary(yc_model)
-
-
-
-yc_model_reduced <- update(yc_model, . ~ . - chsex:agemon:drwaterq_new)
+yc_model_reduced <- lmer(zhfa ~ chsex * time + chsex * drwaterq_new + time * drwaterq_new +
+                           age_first_c +
+                           (1 | childid),
+                         data = ethiopia_yg,
+                         REML = FALSE)
 anova(yc_model, yc_model_reduced)
 
+# For the younger cohort, the three-way interaction did NOT improve model fit.
+# Height-for-age still varies by sex, water access, and their two-way combinations,
+# The slope over time differs slightly for boys and girls, but this difference
+# does NOT depend on water access in the younger cohort.
 
 
 
-tab_model(full_model, oc_model, yc_model,
-          show.ci = TRUE,
+tab_model(base_model, oc_model, yc_model,
+          show.ci = F,
           show.se = TRUE,
           dv.labels = c("Pooled", "Older", "Younger"))
 
 ######
-#The older cohort shows stronger and more interpretable effects, especially for
-##sex and water quality.
-#The younger cohort appears more homogeneous, with weaker or
-##non-significant effects.
-#The 3-way interaction is only meaningful in the older cohort
-#Interventions targeting water quality may be more impactful for older children,
-##especially boys.
+# Across the full sample and the older cohort, the three-way interaction 
+# (sex × time × water access) meaningfully improves model fit, indicating that 
+# growth trajectories differ by sex, water access, and their combined effect. 
+# In both groups, the slope over time is different for boys and girls, and this 
+# difference depends on whether they have access to safe water.
 
-  
+# In contrast, the younger cohort shows no evidence that the three-way interaction 
+# improves model fit. Growth patterns vary by sex and water access through two-way 
+# interactions, but the combined three-way effect is not meaningful. The difference 
+# in growth slopes between boys and girls does NOT depend on water access in the 
+# younger cohort.
+
+# Overall: the three-way interaction is important for older children and the full 
+# sample, but not for younger children.
+
+
+#################### Model Interaction term selection ####################
+base_model<- lmer(zhfa ~ chsex + time + age_first_c + drwaterq_new +
+                  chsex*time*drwaterq_new +
+                  (1 | childid),
+                data = ethiopia_df,
+                REML = FALSE)
+summary(base_model)
+
+twoway_model<- lmer(zhfa ~ chsex + time + age_first_c + drwaterq_new+ chsex*time +
+                  chsex*drwaterq_new + time*drwaterq_new +
+                  (1 | childid),
+                data = ethiopia_df,
+                REML = FALSE)
+summary(twoway_model)
+
+nointeraction_model <- lmer(zhfa ~ chsex + time + age_first_c + drwaterq_new+
+                         (1 | childid),
+                       data = ethiopia_df,
+                       REML = FALSE)
+
+model_names <- c("No interaction", "Two-way interaction", "Three-way interaction")
+
+library(knitr)
+
+model_table <- data.frame(
+  Model = model_names,
+  AIC = c(AIC(nointeraction_model), AIC(twoway_model), AIC(base_model)),
+  BIC = c(BIC(nointeraction_model), BIC(twoway_model), BIC(base_model))
+)
+
+kable(model_table, digits = 2, caption = "Model Comparison Using AIC and BIC")
