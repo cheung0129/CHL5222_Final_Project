20862bf5ad8bceee7dde62266a53dec3f1794c24
wendy-shue <wendy.shue@mail.utoronto.ca>
2026-04-03 10:55:34 -0400

    added table_description r file

---
+++ R/table_description.R
@@ -0,0 +1,26 @@
+setwd(here())
+## data cleaning 
+ethiopia <- read.csv(paste0(here(),"/data/processed/ethiopia_processed.csv")) |> 
+  dplyr::select(!childid) |> 
+  mutate(chsex=factor(chsex,level=c(1,2), label=c("F","M")))
+yc <- ethiopia |> dplyr::filter(yc==1) |> dplyr::select(round,agemon,chsex,zhfa, drwaterq_new)
+oc <- ethiopia |> dplyr::filter(yc==0) |> dplyr::select(round,agemon,chsex,zhfa, drwaterq_new)
+labels <- list(agemon="Age (months)", chsex="Sex", 
+               zhfa="Height-for-age Z score", drwaterq_new="Access to safe water")
+
+## younger cohort 
+p1 <- tbl_summary(yc, by=round,
+                  missing_text = "Missing", missing_stat = "{p_miss}%",
+                  statistic = all_continuous() ~ "{mean} ({min}, {max})",
+                  label=labels) 
+
+
+## older cohort 
+p2 <- tbl_summary(oc, by=round,
+                  missing_text = "Missing", missing_stat = "{p_miss}%",
+                  statistic = all_continuous() ~ "{mean} ({min}, {max})",
+                  label=labels) 
+
+## combined
+tbl_merge(list(p1,p2), tab_spanner=c("**Younger Cohort**", "**Older Cohort**")) |> 
+  as_kable(format="latex")
